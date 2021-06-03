7f6e2ad8f1affdffe9a1a5006a50c302cd9ce4ec
TarasovDmytro <tarasov.personal@gmail.com>
2021-06-04 01:09:29 +0300

    HW10

---
+++ src/main/java/alevel/ReportInterface.java
@@ -0,0 +1,19 @@
+package alevel;
+
+@FunctionalInterface
+public interface ReportInterface {
+
+    String header = "Header";
+    String footer = "Footer";
+
+    String getBody(String body);
+
+    default String getHeader() {
+        return header;
+    }
+
+    default String getFooter() {
+        return footer;
+    }
+
+}
