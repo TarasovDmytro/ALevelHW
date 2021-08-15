863a8c8c74a6ade36a9c506a38382ee60337444e
TarasovDmytro <tarasov.personal@gmail.com>
2021-08-15 11:25:34 +0300

    Heroku project HW28

--- src/main/java/ua/musicManager/src/main/java/dao/AlbumDao.java
+++ src/main/java/ua/musicManager/src/main/java/dao/AlbumDao.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class AlbumDao {
 
-    public void createInstance(Album album) {
+    public Album createInstance(Album album) {
 
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
@@ -25,6 +25,7 @@ public class AlbumDao {
             }
             e.printStackTrace();
         }
+        return album;
     }
 
     public void updateInstance(Album album) {
@@ -40,6 +41,7 @@ public class AlbumDao {
             }
             e.printStackTrace();
         }
+        HibernateUtil.shutdown();
     }
 
     public Album getInstanceById(int id) {
@@ -95,7 +97,7 @@ public class AlbumDao {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             String hql = "DELETE FROM entities.Album ";
-            Query query = session.createQuery(hql);
+            var query = session.createQuery(hql);
             int result = query.executeUpdate();
             System.out.println("Rows affected: " + result);
             transaction.commit();
@@ -107,20 +109,32 @@ public class AlbumDao {
         }
     }
 
-    public void addTrackToAlbum(Album album, Track track) {
+    public Album addTracksToAlbum(Album album, List<Track> newTracks) {
 
-        if (!album.getTracks().contains(track)) {
-            Artist artist = track.getArtist();
-            album.getTracks().add(track);
-            album.setPrice(album.getPrice() + track.getPrice());
-            album.getArtists().add(artist);
-            updateInstance(album);
-        } else {
-            System.out.println("This track is already on this album");
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            newTracks.forEach(track -> {
+                        Artist artist = track.getArtist();
+                        album.getTracks().add(track);
+                        album.setPrice(album.getPrice() + track.getPrice());
+                        if (!album.getArtists().contains(artist)) {
+                            album.getArtists().add(artist);
+                            artist.getAlbums().add(album);
+                        }
+                    });
+            session.update(album);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
+        return album;
     }
 
-    public void deleteTrackFromAlbum(Album album, Track track) {
+    public Album deleteTrackFromAlbum(Album album, Track track) {
 
         if (album.getTracks().contains(track)) {
             album.getTracks().remove(track);
@@ -129,5 +143,6 @@ public class AlbumDao {
         } else {
             System.out.println("There is no such track in this album");
         }
+        return album;
     }
 }
