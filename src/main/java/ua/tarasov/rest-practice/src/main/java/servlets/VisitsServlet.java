32c67b33e0e0c59c1aef32b43eb215e461f587da
TarasovDmytro <tarasov.personal@gmail.com>
2021-09-05 19:20:37 +0300

    some change for best visualisation

--- src/main/java/ua/tarasov/rest-practice/src/main/java/servlets/VisitsServlet.java
+++ src/main/java/ua/tarasov/rest-practice/src/main/java/servlets/VisitsServlet.java
@@ -23,8 +23,13 @@ public class VisitsServlet extends HttpServlet {
         Statistic.getStatistic();
 
         List<Visit> visits = service.getVisits();
-        resp.setStatus(200);
-        respBody.println(new Gson().toJson(visits));
+        if (visits == null) {
+            resp.setStatus(400);
+            respBody.println("Status " + resp.getStatus() + " - No visitors in list");
+        } else {
+            resp.setStatus(200);
+            visits.forEach(visit -> respBody.println(new Gson().toJson(visit)));
+        }
     }
 
     @Override
@@ -36,9 +41,14 @@ public class VisitsServlet extends HttpServlet {
         String visitorName = req.getParameter("name");
         String visitorDate = req.getParameter("date");
         String visitorCity = req.getParameter("city");
-        Visit visit = service.postVisit(visitorName, visitorDate, visitorCity);
 
-        resp.setStatus(200);
-        respBody.println(new Gson().toJson(visit));
+        if (visitorName == null && visitorCity == null && visitorDate == null) {
+            resp.setStatus(400);
+            respBody.println("Status " + resp.getStatus() + " - At least one of the visitor parameters must not be null");
+        } else {
+            Visit visit = service.postVisit(visitorName, visitorDate, visitorCity);
+            resp.setStatus(200);
+            respBody.println(new Gson().toJson(visit));
+        }
     }
 }
